e74eb6e5f46a9a5f1c38e1917ba3b46e036e515d
Xaba73 <101169583+Xaba73@users.noreply.github.com>
2022-11-07 14:18:29 +0300

    chore: добавил компонентов для списка заказов, подготовил модалку для сбора заказа

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Вкусные блюда, доставляем для вас</h2>
+      <p>
+         Выберите свое любимое блюдо из нашего широкого меню
+         и наслаждайтесь вкусным обедом или ужином дома.
+      </p>
+      <p>
+        Все наши блюда готовятся из высококачественных ингредиентов, точно в срок и
+        конечно же опытными поварами!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
