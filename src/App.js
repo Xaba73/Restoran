01b18adc87b7406e126ff163a802bd7c002967f9
Xaba73 <101169583+Xaba73@users.noreply.github.com>
2022-11-08 14:08:13 +0300

    chore: Сделал модальное окно, счётчик блюд в заказе через ref Хук и контекст

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+
+
 
 function App() {
+ const [showCart, setShowCart] = useState(false);
+
+ const isShowHandler = () => {
+  setShowCart(true);
+ }
+
+ const isHideHandler = () => {
+  setShowCart(false);
+ }
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {showCart && <Cart onClose = {isHideHandler}/>}
+      <Header onShowCart={isShowHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber)
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Количество'
         input={{
           id: 'amount_' + props.id,
@@ -16,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Введите корректное количество блюд от 1 до 5</p>}
     </form>
   );
 };
